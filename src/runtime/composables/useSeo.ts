24314858fdb9b7ff18d89ab8affae6948cee6349
tubstrr <me@jonknoll.dev>
2023-12-05 12:32:45 -0600

    update: deepMerge converts arrays into objects

--- src/runtime/composables/useSeo.ts
+++ src/runtime/composables/useSeo.ts
@@ -489,7 +489,7 @@ const initOptions = (options: SeoModuleOptions | boolean = false) => {
 const deepMerge = (target: any, source: any) => {
   const output = Object.assign({}, target);
   for (const key of Object.keys(source)) {
-    if (source[key] instanceof Object)
+    if (source[key] instanceof Object && !Array.isArray(source[key]))
       output[key] = deepMerge(output[key], source[key]);
     else output[key] = source[key];
   }
